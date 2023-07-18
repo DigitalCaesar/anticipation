b6a47a6a8c17459a93f7873e3b72ff1e9535f3cd
Eric DeCesare <eric.decesare@outlook.com>
2023-07-18 08:25:55 -0400

    Added Sample project.  Fixed AB#325 and AB#326

---
+++ Samples/Console.Sample/ErrorStrategy/AverageCalculatorWithErrors.cs
@@ -0,0 +1,24 @@
+﻿namespace DigitalCaesar.Anticipation.Samples;
+
+public class AverageCalculatorWithErrors
+{
+
+    public static AverageResult Calculate(int[] values)
+    {
+        decimal count = values.Length;
+        if(count <= 0)
+            return new AverageResult(0, AverageError.NoValues);
+
+        decimal total = 0;
+        foreach (int value in values)
+        {
+            if (value > 100)
+                return new AverageResult(0, AverageError.ValueTooHigh(value));
+            if (value < 0)
+                return new AverageResult(0, AverageError.ValueTooLow(value));
+            total += value;
+        }
+        decimal result = total / count;
+        return new AverageResult(result, null);
+    }
+}
